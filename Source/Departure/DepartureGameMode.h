b5fb11c407bf196fbcb00b363e2d22eec8a8df6b
Percy K <prkaylor13@gmail.com>
2018-06-25 14:54:53 -0400

    First commit.

---
+++ Source/Departure/DepartureGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DepartureGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADepartureGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADepartureGameMode();
+};
+
+
+
